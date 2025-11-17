ff218d7b7b2d8262cf24eb0bd696e456f66df9d3
Winson Wang <winsonwang03@gmail.com>
2025-11-17 02:31:32 -0500

    final commit for today

---
+++ wordhunt_app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask import render_template
+from backend.solver import *
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        input_grid = request.form['input_grid']
+        results = web_solver(input_grid)
+        return render_template('index.html', results=results)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
